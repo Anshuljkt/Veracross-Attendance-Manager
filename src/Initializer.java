124c6d941f1d8bde0ec5d4a7f165a8038efe4e79
Anshuljkt <anshul.jkt@gmail.com>
2017-07-07 14:06:01 +0530

    First Commit
    Have set up basic Student ArrayList, and have also made it possible to
    take an xml file in, convert it into student objects, and feed into the
    Students arrayList

---
+++ src/Initializer.java
@@ -0,0 +1,50 @@
+import org.w3c.dom.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Created by 18anshula on 7/7/17 at 12:55 PM.
+ */
+public class Initializer {
+
+
+    static ArrayList<Student> students = new ArrayList<Student>();
+
+    public static void main(String[] args) {
+        try {
+            //Creates a new DocumentBuilder to handle the xml file using Java DOM Parser
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new File("xmls/studentsp1.xml"));
+            Element root = document.getDocumentElement();
+//            System.out.println(root.getNodeName());
+
+
+            //Create a temporary list that creates a node for each student object in the xml file received.
+            NodeList tempList = document.getElementsByTagName("student");
+            //for loop that feeds the relevant data into the students ArrayList as new objects.
+            for (int i = 0; i<tempList.getLength(); i++) {
+                Node current = tempList.item(i);
+                if (current.getNodeName().equalsIgnoreCase("student")) {
+                    Element eElement = (Element) current;
+                    int id = Integer.parseInt(eElement.getElementsByTagName("person_pk").item(0).getTextContent());
+                    String fName = eElement.getElementsByTagName("first_name").item(0).getTextContent();
+                    String pName = eElement.getElementsByTagName("preferred_name").item(0).getTextContent();
+                    if (pName.equals("")) {pName=fName;}
+                    String lName = eElement.getElementsByTagName("last_name").item(0).getTextContent();
+                    String email = eElement.getElementsByTagName("email_1").item(0).getTextContent();
+                    int homeroom = Integer.parseInt(eElement.getElementsByTagName("homeroom").item(0).getTextContent());
+                    String grade = eElement.getElementsByTagName("current_grade").item(0).getTextContent();
+
+                    students.add(new Student(fName, lName, id, email, homeroom, grade));
+                }
+                System.out.println(students.get(i));
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,96 @@
+/**
+ * Created by Anshul Agrawal on 7/7/17 at 12:50 PM.
+ */
+public class Student {
+    
+
+    public Student() {
+    }
+
+
+    public Student(String fName, String lName, int id, String email, int homeroom, String grade) {
+        this.fName = fName;
+        this.lName = lName;
+        this.id = id;
+        this.email = email;
+        this.homeroom = homeroom;
+        this.grade = grade;
+        this.gradeNum = Integer.parseInt(grade.substring(5));
+    }
+    private String fName;
+    private String lName;
+    private int id;
+    private String email;
+    private int homeroom;
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+        this.gradeNum = Integer.parseInt(grade.substring(5));
+    }
+
+    public int getGradeNum() {
+        return gradeNum;
+    }
+
+    public void setGradeNum(int gradeNum) {
+        this.gradeNum = gradeNum;
+        this.grade = "Year " + gradeNum;
+    }
+
+    private String grade;
+    private int gradeNum;
+
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getHomeroom() {
+        return homeroom;
+    }
+
+    public void setHomeroom(int homeroom) {
+        this.homeroom = homeroom;
+    }
+
+    public String toString() {
+        return  "First Name = " + fName +
+                " | Last Name = " + lName +
+                " | Grade = " + gradeNum +
+                " | ID = " + id +
+                " | Email = " + email +
+                " | Homeroom ID = " + homeroom;
+    }
+}
